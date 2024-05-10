f19697b7376d263354f32df248445e5725a65d27
nitheeshks03 <nitheeshks98@gmail.com>
2024-05-10 21:53:11 +0530

    total

--- internal_mess_tracker.rb
+++ internal_mess_tracker.rb
@@ -35,6 +35,23 @@ def meal(meal_log)
   return [b, l, d].reject(&:empty?).join(", ")
 end
 
+def day_total(meal_log)
+  b = 40
+  l = 70
+  d = 40
+  total = 0
+
+  if meal_log[:breakfast] && meal_log[:lunch] && meal_log[:dinner]
+    total = 130
+  else
+    total += b if meal_log[:breakfast]
+    total += l if meal_log[:lunch]
+    total += d if meal_log[:dinner]
+  end
+
+  return total
+end
+
 get "/" do
   if User.count.zero?
     @mess_users = ["Nitheesh", "Pranav", "Abhiraj", "Rahul", "Aswin", "Demo", "Vishak"]
@@ -50,6 +67,12 @@ get "/meal_logs" do
   user_id = params[:user_id]
 
   @meal_logs = MealLog.joins(:user).select(:breakfast, :lunch, :dinner, :meal_date).where("users.id = ?", user_id)
+  @month_total = 0
+
+  @meal_logs.each do |meal_log|
+    @month_total += day_total(meal_log)
+  end
+
   erb :meal_logs
 end
 
